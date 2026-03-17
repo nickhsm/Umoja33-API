99f63958f2581b5e6063891b354effc50ac6ada3
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2026-03-17 12:52:05 +0100

    Post should work

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,11 @@
+from typing import Annotated
+
 from fastapi import FastAPI
+from sqlmodel import Session, select
+
+from .database.orm.weather import DataPoint, WeatherStation
+from .database.database_connection import engine
+
 from .models.weather_post import Weather
 
 app = FastAPI()
@@ -10,4 +17,22 @@ async def root():
 
 @app.post("/weather")
 def post_weather_data(weather: Weather):
-    return weather.sensors
+    # Get weatherstation_id
+    with Session(engine) as session:
+        statement = select(WeatherStation).where(WeatherStation.station_id == weather.station_id)
+        weatherstation_id = session.exec(statement)
+
+    data = DataPoint(
+            temperature=weather.sensors.temperature,
+            humidity=weather.sensors.humidity,
+            wind_speed=weather.sensors.wind_speed,
+            wind_direction=weather.sensors.wind_direction,
+            precipitation=weather.sensors.precipitation,
+            timestamp=weather.timestamp,
+            weatherstation_id=weatherstation_id
+            )
+
+    with Session(engine) as session:
+        session.add(data)
+
+    return {"Status": "success"}
